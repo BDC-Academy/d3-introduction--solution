1a08cdd399c9ee0e21672089798f676a8fc26c28
bdca-marcel <marcel@b21s.nl>
2022-06-15 16:12:02 +0200

    remove uneccessary files

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-function calculateContentWidth(svgWidth, margin) {
-   return svgWidth - margin.left - margin.right;
-}
-
-function calculateContentHeight(svgHeight, margin) {
-  return svgHeight - margin.top - margin.bottom;
-}
